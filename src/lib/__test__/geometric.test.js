75b8b1d1488e0c0e87f74b9b93256f558bd89362
Mr. Wason <rohit.wason@jefferson.kyschools.us>
2024-05-27 00:16:57 -0400

    problem 45

--- src/lib/__test__/geometric.test.js
+++ src/lib/__test__/geometric.test.js
@@ -6,5 +6,20 @@ describe("Geometric", () => {
     expect(subject.triangle(2)).toEqual(3);
     expect(subject.triangle(7)).toEqual(28);
   });
+  it("Squares", () => {
+    expect(subject.square(1)).toEqual(1);
+    expect(subject.square(2)).toEqual(4);
+    expect(subject.square(7)).toEqual(49);
+  });
+  it("Pentagonal", () => {
+    expect(subject.pentagonal(1)).toEqual(1);
+    expect(subject.pentagonal(2)).toEqual(5);
+    expect(subject.pentagonal(7)).toEqual(70);
+  });
+  it("Hexagonal", () => {
+    expect(subject.hexagonal(1)).toEqual(1);
+    expect(subject.hexagonal(2)).toEqual(6);
+    expect(subject.hexagonal(7)).toEqual(91);
+  });
 });
 
--- src/problems/problem-45.js
+++ src/problems/problem-45.js
@@ -1,6 +1,54 @@
 //Euler Problem 45
 //https://projecteuler.net/problem=45
 
+const G = require("../lib/geometric");
+
 module.exports.run = function () {
+    var _t = 1;
+    var _p = 1;
+    var _h = 1;
+
+    var tf = true;
+    var pf = true;
+    var hf = true;
     
+    while (true) {
+        if (tf) tv = G.triangle(_t);
+        if (pf) pv = G.pentagonal(_p);
+        if (hf) hv = G.hexagonal(_h);
+
+        if (tv > pv && tv > hv) {
+            tf = false;
+            pf = true;
+            hf = true;
+            _p++;
+            _h++;
+        }else if (pv > tv && pv > hv) {
+            tf = true;
+            pf = false;
+            hf = true;
+            _t++;
+            _h++;
+        } else if (hv > tv && hv > pv) {
+            tf = true;
+            pf = true;
+            hf = false;
+            _t++;
+            _p++;
+        } else {
+            if (tv == pv && pv == hv) {
+                console.log({tv, _t, _p, _h});
+            }
+
+            tf = true;
+            pf = true;
+            hf = true;
+
+            _t++;
+            _p++;
+            _h++;
+        }
+
+        // console.log({_t, _p, _h});
+    }
 }
