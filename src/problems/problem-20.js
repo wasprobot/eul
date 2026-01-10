66892f09f9097afc4e4f692dd5064647ffb2577c
Rohit Wason <wasonrohit@gmail.com>
2026-01-09 21:15:42 -0500

    problem 20

--- src/problems/problem-20.js
+++ src/problems/problem-20.js
@@ -5,17 +5,30 @@ const Time = require("../lib/time");
 
 module.exports.run = function (args) {
   let max = args || 100;
-  factorials = [1];
-  for (let i = 1; i <= max; i++) {
-    factorials.push(factorials[i - 1] * i);
+  factorials = [1n];
+  for (let i = 1n; i <= max; i++) {
+    factorials.push(factorials[i - 1n] * i);
   }
+  console.log({ factorials });
+
   let last = factorials[max];
-  let sum = 0;
+  let sum = 0n;
+
   while (last > 0) {
-    let digit = last % 10;
-    console.log({ digit });
-    sum += last % 10;
-    last = Math.floor(last / 10);
+    const remainder = last % 10n;
+    const quotient = last / 10n;
+
+    sum += remainder;
+
+    // If the result is negative and there's 
+    // a non-zero remainder, round down 
+    // further (towards negative infinity).
+    if (quotient < 0n && remainder !== 0n) {
+      last = quotient - 1n;
+    } else {
+      last = quotient;
+    }
   }
+
   console.log({ sum });
 };
